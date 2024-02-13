2f4745f9f7d011ee24fc55fcd10412f696024b19
Krisztián Csillag <christiaanhoijtink@mcb-ch-mxd.home>
2024-02-13 10:59:42 +0100

    complete redesign on the wheel

--- example/src/App.js
+++ example/src/App.js
@@ -4,45 +4,135 @@ import WheelComponent from './indexCopy'
 import './index.css'
 
 const App = () => {
-
-  const wheelRef = useRef();
+  const wheelRef = useRef()
   const [segments, setSegments] = useState([
-    'better luck next time',
-    'won 70',
-    'won 10',
-    'better luck next time',
-    'won 2',
-    'won uber pass',
-    'better luck next time',
-    'won a voucher',
-    'won 70',
-    'won 10',
-    'better luck next time',
-    'won 2',
-    'won uber pass',
-    'better luck next time',
-    'won a voucher'
+    {
+      text: 'Blood Rage',
+      image:
+        'https://cf.geekdo-images.com/HkZSJfQnZ3EpS214xtuplg__original/img/Myy6IPDJDzLoPdXrPXVZcddBQoQ=/0x0/filters:format(jpeg)/pic2439223.jpg'
+    },
+    {
+      text: 'Rising Sun',
+      image:
+        'https://cf.geekdo-images.com/iwevA6XmiNLHn1QnGUucqw__original/img/pKJ1XyLN4hvHim5eRj2VJHijv1E=/0x0/filters:format(jpeg)/pic3880340.jpg'
+    },
+    {
+      text: 'Ankh: Gods of Egypt',
+      image:
+        'https://cf.geekdo-images.com/_al0scMG_pQfGVM31Scf1Q__original/img/h4jBTaOjznJgWELa6tTrfPqqSeA=/0x0/filters:format(jpeg)/pic6107853.jpg'
+    },
+    {
+      text: 'Blood Rage',
+      image:
+        'https://cf.geekdo-images.com/HkZSJfQnZ3EpS214xtuplg__original/img/Myy6IPDJDzLoPdXrPXVZcddBQoQ=/0x0/filters:format(jpeg)/pic2439223.jpg'
+    },
+    {
+      text: 'Rising Sun',
+      image:
+        'https://cf.geekdo-images.com/iwevA6XmiNLHn1QnGUucqw__original/img/pKJ1XyLN4hvHim5eRj2VJHijv1E=/0x0/filters:format(jpeg)/pic3880340.jpg'
+    },
+    {
+      text: 'Ankh: Gods of Egypt',
+      image:
+        'https://cf.geekdo-images.com/_al0scMG_pQfGVM31Scf1Q__original/img/h4jBTaOjznJgWELa6tTrfPqqSeA=/0x0/filters:format(jpeg)/pic6107853.jpg'
+    },
+    {
+      text: 'Blood Rage',
+      image:
+        'https://cf.geekdo-images.com/HkZSJfQnZ3EpS214xtuplg__original/img/Myy6IPDJDzLoPdXrPXVZcddBQoQ=/0x0/filters:format(jpeg)/pic2439223.jpg'
+    },
+    {
+      text: 'Rising Sun',
+      image:
+        'https://cf.geekdo-images.com/iwevA6XmiNLHn1QnGUucqw__original/img/pKJ1XyLN4hvHim5eRj2VJHijv1E=/0x0/filters:format(jpeg)/pic3880340.jpg'
+    },
+    {
+      text: 'Ankh: Gods of Egypt',
+      image:
+        'https://cf.geekdo-images.com/_al0scMG_pQfGVM31Scf1Q__original/img/h4jBTaOjznJgWELa6tTrfPqqSeA=/0x0/filters:format(jpeg)/pic6107853.jpg'
+    },
+    {
+      text: 'Blood Rage',
+      image:
+        'https://cf.geekdo-images.com/HkZSJfQnZ3EpS214xtuplg__original/img/Myy6IPDJDzLoPdXrPXVZcddBQoQ=/0x0/filters:format(jpeg)/pic2439223.jpg'
+    },
+    {
+      text: 'Rising Sun',
+      image:
+        'https://cf.geekdo-images.com/iwevA6XmiNLHn1QnGUucqw__original/img/pKJ1XyLN4hvHim5eRj2VJHijv1E=/0x0/filters:format(jpeg)/pic3880340.jpg'
+    },
+    {
+      text: 'Ankh: Gods of Egypt',
+      image:
+        'https://cf.geekdo-images.com/_al0scMG_pQfGVM31Scf1Q__original/img/h4jBTaOjznJgWELa6tTrfPqqSeA=/0x0/filters:format(jpeg)/pic6107853.jpg'
+    },
+    {
+      text: 'Blood Rage',
+      image:
+        'https://cf.geekdo-images.com/HkZSJfQnZ3EpS214xtuplg__original/img/Myy6IPDJDzLoPdXrPXVZcddBQoQ=/0x0/filters:format(jpeg)/pic2439223.jpg'
+    },
+    {
+      text: 'Rising Sun',
+      image:
+        'https://cf.geekdo-images.com/iwevA6XmiNLHn1QnGUucqw__original/img/pKJ1XyLN4hvHim5eRj2VJHijv1E=/0x0/filters:format(jpeg)/pic3880340.jpg'
+    },
+    {
+      text: 'Ankh: Gods of Egypt',
+      image:
+        'https://cf.geekdo-images.com/_al0scMG_pQfGVM31Scf1Q__original/img/h4jBTaOjznJgWELa6tTrfPqqSeA=/0x0/filters:format(jpeg)/pic6107853.jpg'
+    },
+    {
+      text: 'Blood Rage',
+      image:
+        'https://cf.geekdo-images.com/HkZSJfQnZ3EpS214xtuplg__original/img/Myy6IPDJDzLoPdXrPXVZcddBQoQ=/0x0/filters:format(jpeg)/pic2439223.jpg'
+    },
+    {
+      text: 'Rising Sun',
+      image:
+        'https://cf.geekdo-images.com/iwevA6XmiNLHn1QnGUucqw__original/img/pKJ1XyLN4hvHim5eRj2VJHijv1E=/0x0/filters:format(jpeg)/pic3880340.jpg'
+    },
+    {
+      text: 'Ankh: Gods of Egypt',
+      image:
+        'https://cf.geekdo-images.com/_al0scMG_pQfGVM31Scf1Q__original/img/h4jBTaOjznJgWELa6tTrfPqqSeA=/0x0/filters:format(jpeg)/pic6107853.jpg'
+    },
+    {
+      text: 'Blood Rage',
+      image:
+        'https://cf.geekdo-images.com/HkZSJfQnZ3EpS214xtuplg__original/img/Myy6IPDJDzLoPdXrPXVZcddBQoQ=/0x0/filters:format(jpeg)/pic2439223.jpg'
+    },
+    {
+      text: 'Rising Sun',
+      image:
+        'https://cf.geekdo-images.com/iwevA6XmiNLHn1QnGUucqw__original/img/pKJ1XyLN4hvHim5eRj2VJHijv1E=/0x0/filters:format(jpeg)/pic3880340.jpg'
+    },
+    {
+      text: 'Ankh: Gods of Egypt',
+      image:
+        'https://cf.geekdo-images.com/_al0scMG_pQfGVM31Scf1Q__original/img/h4jBTaOjznJgWELa6tTrfPqqSeA=/0x0/filters:format(jpeg)/pic6107853.jpg'
+    },
+    {
+      text: 'Blood Rage',
+      image:
+        'https://cf.geekdo-images.com/HkZSJfQnZ3EpS214xtuplg__original/img/Myy6IPDJDzLoPdXrPXVZcddBQoQ=/0x0/filters:format(jpeg)/pic2439223.jpg'
+    },
+    {
+      text: 'Rising Sun',
+      image:
+        'https://cf.geekdo-images.com/iwevA6XmiNLHn1QnGUucqw__original/img/pKJ1XyLN4hvHim5eRj2VJHijv1E=/0x0/filters:format(jpeg)/pic3880340.jpg'
+    },
+    {
+      text: 'Ankh: Gods of Egypt',
+      image:
+        'https://cf.geekdo-images.com/_al0scMG_pQfGVM31Scf1Q__original/img/h4jBTaOjznJgWELa6tTrfPqqSeA=/0x0/filters:format(jpeg)/pic6107853.jpg'
+    },
+    {
+      text: 'Rising Sun',
+      image:
+        'https://cf.geekdo-images.com/iwevA6XmiNLHn1QnGUucqw__original/img/pKJ1XyLN4hvHim5eRj2VJHijv1E=/0x0/filters:format(jpeg)/pic3880340.jpg'
+    }
   ])
 
-  console.log(segments)
-
-  const segColors = [
-    '#EE4040',
-    '#F0CF50',
-    '#815CD1',
-    '#3DA5E0',
-    '#34A24F',
-    '#F9AA1F',
-    '#EC3F3F',
-    '#FF9000',
-    '#F0CF50',
-    '#815CD1',
-    '#3DA5E0',
-    '#34A24F',
-    '#F9AA1F',
-    '#EC3F3F',
-    '#FF9000'
-  ]
   const onFinished = (winner) => {
     //console.log(winner)
   }
@@ -51,34 +141,47 @@ const App = () => {
       <WheelComponent
         ref={wheelRef}
         segments={segments}
-        segColors={segColors}
         onFinished={(winner) => onFinished(winner)}
-        primaryColor='black'
-        contrastColor='white'
         buttonText='Spin'
         isOnlyOnce={false}
         size={290}
-        upDuration={100}
-        downDuration={1000}
       />
-      <button onClick={()=>{
-          wheelRef.current.spin(segments)
-        }}>SPIN</button>
-        <button onClick={()=>{
-          setSegments(state=>{
-          const newSegments = ([...state, 'addNew'])
-          wheelRef.current.redraw(newSegments)
-          return newSegments
+      <button
+        onClick={() => {
+          wheelRef.current.spin()
+        }}
+      >
+        SPIN
+      </button>
+      <button
+        onClick={() => {
+          setSegments((state) => {
+            const newSegments = [
+              ...state,
+              {
+                text: 'addNew',
+                image:
+                  'https://cf.geekdo-images.com/_al0scMG_pQfGVM31Scf1Q__original/img/h4jBTaOjznJgWELa6tTrfPqqSeA=/0x0/filters:format(jpeg)/pic6107853.jpg'
+              }
+            ]
+            wheelRef.current.redraw(newSegments)
+            return newSegments
           })
-          
-        }}>+</button>
-        <button onClick={()=>{
-          setSegments(state=>{
-          const newSegments = ([...state.slice(0, state.length-1)])
-          wheelRef.current.redraw(newSegments)
-          return newSegments
+        }}
+      >
+        +
+      </button>
+      <button
+        onClick={() => {
+          setSegments((state) => {
+            const newSegments = [...state.slice(0, state.length - 1)]
+            wheelRef.current.redraw(newSegments)
+            return newSegments
           })
-        }}>-</button>
+        }}
+      >
+        -
+      </button>
     </React.Fragment>
   )
 }
--- src/index.js
+++ src/index.js
@@ -5,60 +5,90 @@ import React, {
   useImperativeHandle
 } from 'react'
 
-function adjust(color, amount) {
-  return (
-    '#' +
-    color
-      .replace(/^#/, '')
-      .replace(/../g, (color) =>
-        (
-          '0' +
-          Math.min(255, Math.max(0, parseInt(color, 16) + amount)).toString(16)
-        ).substr(-2)
-      )
-  )
-}
-
 const WheelComponent = forwardRef(
   (
     {
       canvasStyle = {},
-      segments,
-      segColors,
+      segments: list,
       winningSegment,
       onFinished,
-      primaryColor = 'black',
-      contrastColor = 'white',
-      buttonText = 'Spin',
+      primaryColor = '#171D1C',
+      contrastColor = '#16BAC5',
       isOnlyOnce = true,
       size = 290,
-      upDuration = 100,
-      downDuration = 1000,
-      fontFamily = 'proxima-nova'
+      fontFamily = 'Verdana (sans-serif)'
     },
     ref
   ) => {
-    let currentSegment = { text: '', key: 0 }
-    const [isFinished, setFinished] = useState(false)
+    let currentSegment = { text: '', image: '', key: -1 }
     let timerHandle = 0
     let angleCurrent = 0
     let angleDelta = 0
     let canvasContext = null
     let spinStart = 0
     let frames = 0
+
     const centerX = 300
     const centerY = 300
+    const segColors = [
+      '#F8333C',
+      '#7D53DE',
+      '#094D92',
+      '#44AF69',
+      '#FEE440',
+      '#F9A03F'
+    ]
+
+    const [isFinished, setFinished] = useState(false)
+    const [loaded, setLoaded] = useState(false)
+    const [segments, setSegments] = useState(list)
+    const [images, setImages] = useState([])
+
     useEffect(() => {
-      wheelInit(segments)
-      setTimeout(() => {
-        window.scrollTo(0, 1)
-      }, 0)
-    }, [])
+      const imageURL = segments.map((item) => item.image)
+      let imageCount = 0
+      const newImages = [...images]
+      imageURL.forEach((src) => {
+        const image = new Image()
+        image.src = src
+        image.onload = () => {
+          imageCount += 1
+          if (imageCount === imageURL.length) {
+            setLoaded(true)
+          }
+        }
+        newImages.push(image)
+        setImages(newImages)
+        setLoaded(false)
+      })
+    }, [segments])
+
+    useEffect(() => {
+      if (loaded) {
+        const loadedWithImages = list.map((item, index) => {
+          return { ...item, image: images[index] }
+        })
+        setSegments(loadedWithImages)
+        setImages([])
+        wheelInit(loadedWithImages)
+        setTimeout(() => {
+          window.scrollTo(0, 1)
+        }, 0)
+      }
+    }, [loaded])
+
+    useEffect(() => {
+      if (loaded) {
+        initCanvas()
+        wheelDraw(segments)
+      }
+    }, [segments])
+
     const wheelInit = (segments) => {
-      initCanvas(segments)
+      initCanvas()
       wheelDraw(segments)
     }
-    const initCanvas = (segments) => {
+    const initCanvas = () => {
       let canvas = document.getElementById('canvas')
       if (navigator.userAgent.indexOf('MSIE') !== -1) {
         canvas = document.createElement('canvas')
@@ -76,22 +106,24 @@ const WheelComponent = forwardRef(
     }
 
     useImperativeHandle(ref, () => ({
-      spin: (segments) => {
-        initCanvas(segments)
+      spin: () => {
+        initCanvas()
         spin(segments)
       },
       redraw: (segments) => {
-        initCanvas(segments)
-        wheelDraw(segments)
+        setSegments(segments)
+        setLoaded(false)
+        setImages([])
       }
     }))
 
     const spin = (segments) => {
+      console.log(segments)
       if (timerHandle === 0) {
         spinStart = new Date().getTime()
         const maxSpeed = Math.PI / `${segments.length}`
         frames = 0
-        draw(segments)
+        wheelDraw(segments)
         const timerDelay = segments.length
         timerHandle = setInterval(() => {
           onTimerTick(segments, maxSpeed)
@@ -103,8 +135,23 @@ const WheelComponent = forwardRef(
       const duration = new Date().getTime() - spinStart
       let progress = 0
       let finished = false
-      const upTime = segments.length * upDuration
-      const downTime = segments.length * downDuration
+
+      const x1 = 6
+      const y1Uptime = 30
+      const y1Downtime = 1000
+      const x2 = 30
+      const y2Uptime = 8
+      const y2Downtime = 400
+      const x = segments.length
+
+      const uptimeHelper =
+        y1Uptime + ((x - x1) * (y2Uptime - y1Uptime)) / (x2 - x1)
+      const downtimeHelper =
+        y1Downtime + ((x - x1) * (y2Downtime - y1Downtime)) / (x2 - x1)
+
+      const upTime = segments.length * uptimeHelper
+      const downTime = segments.length * downtimeHelper
+
       if (duration < upTime) {
         progress = duration / upTime
         angleDelta = maxSpeed * Math.sin((progress * Math.PI) / 2)
@@ -126,13 +173,21 @@ const WheelComponent = forwardRef(
           }
         } else {
           progress = duration / downTime
+
+          const x1 = 6
+          const y1Progress = 30
+          const x2 = 30
+          const y2Progress = 1
+          const x = segments.length
+          const progressHelper =
+            y1Progress + ((x - x1) * (y2Progress - y1Progress)) / (x2 - x1)
           angleDelta =
-            (maxSpeed / (progress * 10)) *
+            (maxSpeed / (progress * progressHelper)) *
             Math.sin((progress * Math.PI) / 2 + Math.PI / 2)
         }
         if (progress >= 1) finished = true
       }
-      draw(segments)
+      wheelDraw(segments)
       angleCurrent += angleDelta
       while (angleCurrent >= Math.PI * 2) angleCurrent -= Math.PI * 2
       if (finished) {
@@ -150,31 +205,52 @@ const WheelComponent = forwardRef(
       drawNeedle(segments)
     }
 
-    const draw = (segments) => {
-      clear()
-      drawWheel(segments)
-      drawNeedle(segments)
-    }
-
     const drawSegment = (segments, key, lastAngle, angle) => {
       const ctx = canvasContext
-      const value = segments[key]
+      const { image, text } = segments[key]
+
+      // Draw the segment
       ctx.save()
       ctx.beginPath()
       ctx.moveTo(centerX, centerY)
       ctx.arc(centerX, centerY, size, lastAngle, angle, false)
       ctx.lineTo(centerX, centerY)
       ctx.closePath()
-      ctx.fillStyle =
-        currentSegment.key === key ? adjust(segColors[key], 45) : segColors[key]
+      ctx.fillStyle = segColors[key % segColors.length]
       ctx.fill()
+      ctx.clip()
+
+      // Create the image
+      ctx.translate(centerX, centerY)
+      ctx.rotate((lastAngle + angle) / 2)
+      ctx.translate(-centerX, -centerY)
+      ctx.translate(600, 0)
+      ctx.rotate(Math.PI / 2)
+      ctx.globalAlpha = currentSegment.key === key ? 0.95 : 0.15
+      ctx.translate(150, 15)
+      ctx.scale(0.5, 0.5)
+      ctx.drawImage(image, 0, 0, ctx.canvas.width, ctx.canvas.height)
       ctx.stroke()
+      ctx.restore()
+
+      // Draw the text in the segment
       ctx.save()
       ctx.translate(centerX, centerY)
       ctx.rotate((lastAngle + angle) / 2)
-      ctx.fillStyle = primaryColor
-      ctx.font = 'bold 1em ' + fontFamily
-      ctx.fillText(value.substr(0, 21), size / 2 + 20, 0)
+      ctx.fillStyle = '#EFE9F4'
+      ctx.font =
+        currentSegment.key === key ? 'bold 1em Tahoma' : 'bold 0.8em Tahoma'
+      ctx.shadowColor = primaryColor
+      ctx.shadowBlur = 4
+      ctx.shadowOffsetX = -0.5
+      ctx.shadowOffsetY = 0.5
+      ctx.strokeStyle =
+        currentSegment.key === key
+          ? primaryColor
+          : segColors[key % segColors.length] // Change this to the color you want for the stroke
+      ctx.lineWidth = currentSegment.key === key ? 4 : 3 // Change this to control the width of the stroke
+      ctx.strokeText(text.substr(0, 21), size / 1.7, 0)
+      ctx.fillText(text.substr(0, 21), size / 1.7, 0)
       ctx.restore()
     }
 
@@ -188,7 +264,6 @@ const WheelComponent = forwardRef(
       ctx.textBaseline = 'middle'
       ctx.textAlign = 'center'
       ctx.font = '1em ' + fontFamily
-      console.log('segments', segments)
       for (let i = 1; i <= len; i++) {
         const angle = PI2 * (i / len) + angleCurrent
         drawSegment(segments, i - 1, lastAngle, angle)
@@ -197,24 +272,19 @@ const WheelComponent = forwardRef(
 
       // Draw a center circle
       ctx.beginPath()
-      ctx.arc(centerX, centerY, 40, 0, PI2, false)
+      ctx.arc(centerX, centerY, 35, 0, PI2, false)
       ctx.closePath()
-      ctx.fillStyle = contrastColor
+      ctx.fillStyle = primaryColor
       ctx.lineWidth = 10
       ctx.strokeStyle = primaryColor
       ctx.fill()
-      ctx.font = 'bold 1em ' + fontFamily
-      ctx.fillStyle = primaryColor
-      ctx.textAlign = 'center'
-      ctx.fillText(buttonText, centerX, centerY + 3)
       ctx.stroke()
 
       // Draw outer circle
       ctx.beginPath()
       ctx.arc(centerX, centerY, size, 0, PI2, false)
       ctx.closePath()
-
-      ctx.lineWidth = 10
+      ctx.lineWidth = 13
       ctx.strokeStyle = primaryColor
       ctx.stroke()
     }
@@ -223,11 +293,11 @@ const WheelComponent = forwardRef(
       const ctx = canvasContext
       ctx.lineWidth = 1
       ctx.strokeStyle = primaryColor
-      ctx.fileStyle = primaryColor
+      ctx.fillStyle = primaryColor
       ctx.beginPath()
-      ctx.moveTo(centerX + 20, centerY - 40)
-      ctx.lineTo(centerX - 20, centerY - 40)
-      ctx.lineTo(centerX, centerY - 55)
+      ctx.moveTo(centerX + 13, centerY - 30)
+      ctx.lineTo(centerX - 13, centerY - 30)
+      ctx.lineTo(centerX, centerY - 100)
       ctx.closePath()
       ctx.fill()
       const change = angleCurrent + Math.PI / 2
@@ -240,7 +310,7 @@ const WheelComponent = forwardRef(
       ctx.textBaseline = 'middle'
       ctx.fillStyle = contrastColor
       ctx.font = 'bold 1.5em ' + fontFamily
-      currentSegment = { text: segments[i], key: i }
+      currentSegment = { ...segments[i], key: i }
     }
     const clear = () => {
       const ctx = canvasContext
